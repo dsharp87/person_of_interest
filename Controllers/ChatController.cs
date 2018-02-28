22b449fefd16d9ab72039d6bc759f37f48243680
Per Steenstrup <per.steenstrup@gmail.com>
2018-02-27 16:20:04 -0800

    psuedo code for chathub

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+// using person_of_interest.Models;
+
+namespace person_of_interest.Controllers
+{
+    [Route("[controller]")]
+    public class ChatController : Controller
+    {
+        // private ProjectContext _context;
+
+        // [HttpGet("[action]")]
+        // public IEnumerable<Object> GetSession()
+        // {
+        // }
+    }
+}

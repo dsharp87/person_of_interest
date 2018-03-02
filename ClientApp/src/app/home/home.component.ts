a7eb84a42f2e1e5ce7937f0b87898d13c81bf4a4
dswayne599 <dswayne599@gmail.com>
2018-03-02 00:23:46 -0800

    adding low level css

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   baseUrl:string;
